fea86eeec344f07447db51308a8d95dc8f19a91a
CodeCasterNL <github@codecaster.nl>
2021-09-10 17:18:28 +0200

    feat: allow local debugging

--- docker/Magento2.4/Run.ps1
+++ docker/Magento2.4/Run.ps1
@@ -1,4 +1,7 @@
 # 
 docker-compose up -d
 docker-compose exec --user=root web chown -R kevin:kevin /magento2
-docker-compose exec web php -S 0.0.0.0:5000 -t /magento2/pub/ /magento2/phpserver/router.php
+
+# This starts the site.
+docker-compose exec -e BUZ_API_URL=https://dev.api.boekuwzending.com/ web php -S 0.0.0.0:5000 -t /magento2/pub/ /magento2/phpserver/router.php
+#docker-compose exec web php -S 0.0.0.0:5000 -t /magento2/pub/ /magento2/phpserver/router.php
